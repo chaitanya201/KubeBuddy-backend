7cee61e38aa131fb881f3f12bb80977aa8072d9b
Chaitanya Sawant <chaitanyasawant05072001@gmail.com>
2025-04-26 12:17:24 +0530

    updated error response

--- src/lib/center-function.ts
+++ src/lib/center-function.ts
@@ -1,13 +1,21 @@
 import { NextFunction, Request, Response } from "express";
+import ErrorResponse from "./response/ErrorResponse";
 
 export const catchAsync = (fn: Function) => {
   return (req: Request, res: Response, next: NextFunction) => {
     fn(req, res, next).catch((err: any) => {
       console.error(err);
-      res.status(500).json({
-        status: "error",
-        message: "Internal Server Error",
+      const errorResponse = new ErrorResponse({
+        metadata: {
+          statusCode: 500,
+          message: err instanceof Error ? err.message : "Internal Server Error",
+        },
+        data: {
+          responseData: null,
+        },
       });
+
+      res.status(500).json(errorResponse);
     });
   };
 };
